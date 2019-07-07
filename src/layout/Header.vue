1cde88b61246eb0eab581c62c61d646bfae0eca5
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-07 22:44:55 +0430

    Added props links

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -3,7 +3,11 @@
         <img alt="Lybra" src="../assets/logo.png"/>
         <nav>
             <ul>
-                <li><Link to="#" text="test" /></li>
+                <li><Link to="#" text="Demos" /></li>
+                <li><Link to="#" text="Pages" /></li>
+                <li><Link to="#" text="Elements" /></li>
+                <li><Link to="#" text="Blog" /></li>
+                <li><Link to="#" text="Contact" /></li>
             </ul>
         </nav>
     </header>
