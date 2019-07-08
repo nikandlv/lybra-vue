fb8d8971a48c3eda45d85ba6d81b33f5c0b9c242
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-08 23:39:05 +0430

    Added play button

--- src/layout/Preview.vue
+++ src/layout/Preview.vue
@@ -11,7 +11,9 @@
             </column>
             <column :xs="12" :lg="6">
             <div class="preview-card">
-                 
+                <svg class="play-button" xmlns="http://www.w3.org/2000/svg" width="46.084" height="45.969" viewBox="0 0 46.084 45.969">
+                    <path id="ic_slow_motion_video_24px" d="M27.46,19.929l-7.045-5.29v20.79l7.045-5.29,6.814-5.105Zm0,0-7.045-5.29v20.79l7.045-5.29,6.814-5.105Zm0,0-7.045-5.29v20.79l7.045-5.29,6.814-5.105ZM22.724,6.716V2.05A22.982,22.982,0,0,0,10.435,7.155l3.28,3.3A18.344,18.344,0,0,1,22.724,6.716Zm-12.266,7-3.3-3.28A22.982,22.982,0,0,0,2.05,22.724H6.716a18.344,18.344,0,0,1,3.742-9.009ZM6.716,27.344H2.05A22.982,22.982,0,0,0,7.155,39.634l3.3-3.3a18.175,18.175,0,0,1-3.742-8.986Zm3.719,15.569a23.057,23.057,0,0,0,12.289,5.105V43.353a18.344,18.344,0,0,1-9.009-3.742l-3.28,3.3Zm37.7-17.879A23.135,23.135,0,0,1,27.46,48.019V43.353a18.478,18.478,0,0,0,0-36.637V2.05A23.135,23.135,0,0,1,48.134,25.034Z" transform="translate(-2.05 -2.05)" fill="#fff"/>
+                </svg>
             </div>
             </column>
         </row>
@@ -31,6 +33,15 @@ import Button from '../components/Button'
     background #6c63ff
     width 100%
     height 100%
+    position relative
+    .play-button
+      position absolute
+      top 0
+      left 0
+      bottom 0
+      right 0
+      margin auto
+      cursor pointer
   .preview-text
     padding 10% 0
     max-width 70%
