6ca8bed46a9b3eba9691b56a33e780746ff91157
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-09 23:49:19 +0430

    Added lastposts text section

--- src/layout/LastPosts.vue
+++ src/layout/LastPosts.vue
@@ -1,6 +1,10 @@
 <template>
     <section>
-        
+        <div class="last-post-text">
+            <p class="small-slogan">publication</p>
+            <h1 class="title">Latest posts</h1>
+            <p class="slogan">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unkno...</p>
+        </div>
     </section>
 </template>
 <script>
@@ -8,3 +12,23 @@
      name:"LastPosts"
  }
 </script>
+<style lang="stylus" scoped>
+section
+ padding 0.8vw 0vw
+.last-post-text
+ padding 10% 6rem
+ max-width 55%
+.small-slogan
+ color black
+ text-transform uppercase
+ color lighten(black,30)
+.title
+ color black
+ font-size 2.8rem
+ font-weight bolder
+ margin 1rem 0
+.slogan
+ color black
+ color lighten(black,30)
+ line-height 1.5rem
+</style>
