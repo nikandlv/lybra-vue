2c758d428c1b441d562688e78ebf9608f30fb00b
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-09 23:12:56 +0430

    Added staff cards layout

--- src/layout/Staff.vue
+++ src/layout/Staff.vue
@@ -4,16 +4,30 @@
             <p class="small-slogan">human beings</p>
             <h1 class="title">Our awesome staff</h1>
         </div>
+        <grid class="wrapper">
+            <row>
+                <column :xs="12" :md="6">
+                    <StaffCard />
+                </column>
+                <column :xs="12" :md="6">
+                    <StaffCard class="active" />
+                </column>
+            </row>
+        </grid>
     </section>
 </template>
 <script>
+import StaffCard from '../components/StaffCard';
  export default {
-     name:"Staff"
+     name:"Staff",
+     components:{StaffCard}
  }
 </script>
 <style lang="stylus">
 section
   padding 0.8vw 0vw
+  .wrapper
+    padding 0 8rem
   .features-text
    padding 10% 6rem
    max-width 55%
