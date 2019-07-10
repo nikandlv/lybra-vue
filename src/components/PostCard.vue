1883b211492def379af53d0a34479374fb8e3ac1
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-10 12:54:32 +0430

    Added post card layout and posts images

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,6 +1,20 @@
 <template>
     <div class="card">
-        abc
+        <div class="image">
+            <slot name="image"></slot>
+            <div class="head">
+                <div class="date">
+                    
+                </div>
+                <div class="title">
+                    
+                </div>
+            </div>
+            <h2 class="title">
+                
+            </h2>
+            <a href="#">Read more</a>
+        </div>
     </div>
 </template>
 <script>
